e4cbbe132f51d90976ff8671f34213118d048484
Kanika Gupta <kanika.gupta@appster.in>
2019-02-08 17:35:06 +0530

    Css changes

--- src/app/error.ts
+++ src/app/error.ts
@@ -1,11 +1,11 @@
 export let ERROR_MESSAGES = {
     NO_CONNECTION : "Unable to connect to the server! Please check your network connection and try again.",
     OOPS: "Oops...Something went wrong, please try again later",
-    INVALID_USERNAME: "Sorry, field username is required!",
+    INVALID_USERNAME: "Username is required!",
     INVALID_USERNAME_MINLENGTH: "Sorry, minimum username length is 8!",
     INVALID_USERNAME_MAXLENGTH: "Sorry, maximum username length is 30!",
     INVALID_USERNAME_REGEX: "Sorry, only small and capital letters are allowed!",
-    INVALID_PASSWORD_REQD: "Sorry, field password is required!",
+    INVALID_PASSWORD_REQD: "Password is required!",
     INVALID_PASSWORD: "Password must be more than 8 characters",
     INVALID_CONFIRM_PASSWORD: "The New Password and Confirm New Password didn't matched.",
     INVALID_CURR_PASSWORD: "Please enter a valid password"
